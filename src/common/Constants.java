770598bde78fd47b6727a0e1031ddbad7e29deeb
Iurii Didkovskyi <ydidkovskiy@avertra.com>
2017-08-01 17:11:48 +0300

    Added resources

---
+++ src/common/Constants.java
@@ -0,0 +1,17 @@
+package common;
+
+public interface Constants {
+
+    String[] SOCIAL_DIALOG_SUPPORTED_NETWORKS = {
+            "Twitter"
+    };
+
+    String SOCIAL_DIALOG_TITLE = "Select Social Network";
+    String SOCIAL_DIALOG_OK_ACTION = "Select";
+
+    String AUTH_DIALOG_CREDENTIALS_TITLE = "Enter credentials";
+    String AUTH_HINT_USERNAME = "Username";
+    String AUTH_HINT_PASSWORD = "Password";
+    String AUTH_BTN_AUTHORIZE = "Authorize";
+    String AUTH_BTN_CANCEL = "Cancel";
+}
